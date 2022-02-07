9f97970ddeba617cc86a94e8f7032315845e35d8
Ajay_S <sajayyy01234@gmail.com>
2022-02-07 15:26:54 +0530

    completed header and upfold

---
+++ index.js
@@ -0,0 +1,18 @@
+
+// const open = () => {
+
+    function open() {
+
+    // }
+    const x = document.querySelector(".menu-links");
+
+    if(x.style.display === "block"){
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+
+    // x.style.display = (x.style.display === "block") ? "none" : "block";
+
+
+}
