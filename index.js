b901abc48f0091ec98347665f3c4e7ce941ab845
Ajay_S <sajayyy01234@gmail.com>
2022-02-09 11:40:27 +0530

    added steps fade-in onScroll

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
+const cards = document.querySelectorAll(".card");
+const icons = document.querySelectorAll(".icon");
+const cards1 = document.querySelectorAll(".card-1");
+// console.log(cards);
 
-// const open = () => {
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        // console.log(entry.target.classList[0]);
+        // if(entry.target.classList[0] === "card-1"){
+            // entry.target.classList.toggle("fade-up", entry.isIntersecting);
+        // }else {
+            entry.target.classList.toggle("fade-in", entry.isIntersecting);
+        // }
+        // entry.target.classList.toggle("fade-in", entry.isIntersecting)
+        if(entry.isIntersecting) observer.unobserve(entry.target);
+    })
+    // console.log(entries)
+}, {
+    threshold: 0.8,
+});
 
-    function open() {
+// observer.observe(cards[0]);
 
-    // }
-    const x = document.querySelector(".menu-links");
+cards.forEach(card => {
+    observer.observe(card);
+})
 
-    if(x.style.display === "block"){
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
+icons.forEach(icon => {
+    observer.observe(icon);
+})
 
-    // x.style.display = (x.style.display === "block") ? "none" : "block";
-
-
-}
+// cards1.forEach(card => {
+    // observer.observe(card);
+// })
