5949f127c62145f3db142e8aa2fe2440b92886ef
Ajay_S <sajayyy01234@gmail.com>
2022-02-10 19:04:10 +0530

    mobile view 95%

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 const cards = document.querySelectorAll(".card");
 const icons = document.querySelectorAll(".icon");
 const cards1 = document.querySelectorAll(".card-1");
-// console.log(cards);
+const dots = document.querySelector(".dots").getElementsByTagName("div");
+
+const quotes =  document.querySelector(".quote").getElementsByTagName("li");
+
+const carousel = document.querySelector(".carousel-text").getElementsByTagName("li");
+
+const authors = document.querySelector(".authors").getElementsByTagName("div");
+
+console.log(authors);
 
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
-        // console.log(entry.target.classList[0]);
-        // if(entry.target.classList[0] === "card-1"){
-            // entry.target.classList.toggle("fade-up", entry.isIntersecting);
-        // }else {
-            entry.target.classList.toggle("fade-in", entry.isIntersecting);
-        // }
-        // entry.target.classList.toggle("fade-in", entry.isIntersecting)
+   
+        entry.target.classList.toggle("fade-in", entry.isIntersecting);
+
         if(entry.isIntersecting) observer.unobserve(entry.target);
     })
-    // console.log(entries)
 }, {
     threshold: 0.8,
 });
-
-// observer.observe(cards[0]);
 
 cards.forEach(card => {
     observer.observe(card);
@@ -29,6 +30,46 @@ icons.forEach(icon => {
     observer.observe(icon);
 })
 
-// cards1.forEach(card => {
-    // observer.observe(card);
-// })
+const remove = () => {
+    console.log("hello world");
+    for(let i = 0; i < 3; i++) {
+        dots[i].classList.remove("active");
+        dots[i].style.backgroundColor = "#272727";
+        carousel[i].classList.remove("activeCarousel");
+        quotes[i].classList.remove("activeQuote");
+        authors[i].classList.remove("activeAuthor");
+    } 
+}
+
+const siemaLoader = (selector) => {
+    const mySiema = new Siema({
+        selector: selector,
+        loop: false,
+        perPage: 1,
+        startIndex: 0,
+        onChange: () => remove(),
+    });
+
+    let i = 0;
+    let color = ["#11a59a","#fba737","#f45526"];
+    // console.log(dots);
+    const change = () => {
+        // console.log(i);
+        if(i >= 3) i = 0;
+        mySiema.goTo(i);
+
+            dots[i].style.backgroundColor = color[i];
+            dots[i].classList.add("active");
+            console.log("after");
+            carousel[i].classList.add("activeCarousel");
+            quotes[i].classList.add("activeQuote");
+            authors[i].classList.add("activeAuthor");
+
+        i++;
+    }
+
+    setInterval(change, 3000);
+}
+
+siemaLoader(".carousel-text");
+siemaLoader(".quote");
